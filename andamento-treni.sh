5e5c6df90535d3f0d2421f48c2a3b027a24a066e
matteo <matteo@latitude.group.pirelli.com>
2017-07-12 20:46:50 +0200

    jsonlines for andamento treni

--- andamento-treni.sh
+++ andamento-treni.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-DATE=`date +%Y-%m-%d`
-dir=data/andamento-treni/$DATE
+Ym=`date +%Y/%m`
+DAY=`date +%d`
+dir=data/andamento-treni/$Ym
 
 mkdir -p $dir
+rm  $dir/$DAY.json
 cut -f2 -d'|' data/codici-treno.txt | while read line
 do
   treno=$(echo $line|cut -f1 -d'-')
   stazione=$(echo $line|cut -f2 -d'-')
-  python andamento-treno.py $stazione $treno $dir/$stazione-$treno.json
+  python andamento-treno.py $stazione $treno >> $dir/$DAY.json
 done
--- andamento-treno.py
+++ andamento-treno.py
@@ -7,10 +7,12 @@ sys.setdefaultencoding('utf8')
 
 stazione = sys.argv[1]
 codice = sys.argv[2]
-filename = sys.argv[3]
+#filename = sys.argv[3]
 
 url = "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/andamentoTreno/{stazione}/{codice}"
 with requests.Session() as s:
     r = s.get(url.format(stazione=stazione, codice=codice))
-    with open(filename, "w") as myfile:
-      myfile.write(r.text)
+    #with open(filename, "w") as myfile:
+    text = r.text.replace("\n", "") #+ "\n"
+    #myfile.write(text)
+    print(text)
