e1b792d3ba3dddd4ec0c1640e0625f23f4366bbf
matteoredaelli <matteo.redaelli@gmail.com>
2017-07-13 10:54:18 +0200

    new index

--- stazioni2es.py
+++ stazioni2es.py
@@ -15,11 +15,11 @@ with open(stazionicsv) as data_file:
 
   data = json.load(data_file)
   for entry in data:
-    id = "%s-%s" % (entry["codReg"],entry["codStazione"])
+    id = entry["codStazione"]
     #entry["id"] = id
     logging.warning("stazione %s - %s" % (id, entry["localita"]["nomeLungo"]))
     if azione == "es":
-      es.index(index="grafo", doc_type=u"trenitalia_stazione", id=id, body=entry)
+      es.index(index="grafo", doc_type=u"viaggiatreno_stazione", id=id, body=entry)
     else:
         print("%s,%s,%s" % (entry["codReg"],
                                 entry["codStazione"],
